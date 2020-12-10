57cc242c57ef22588082f91332ec3e1f72bbc8bd
Wolfgang Meier <wolfgang@existsolutions.com>
2020-12-10 09:42:04 +0100

    Fix kbga view link

--- src/connectors/kbga.ts
+++ src/connectors/kbga.ts
@@ -43,7 +43,7 @@ export class KBGA extends Registry {
                 register: this._register,
                 id: item['full-id'],
                 label: item[label],
-                link: `https://meta.karl-barth.ch/${this._register}/${item.id}`
+                link: `https://meta.karl-barth.ch/${register}/${item.id}`
             };
             results.push(result);
         });
